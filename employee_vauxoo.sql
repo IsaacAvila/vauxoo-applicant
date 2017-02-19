1ffe5ada52565a87348683388eff3064ccca93fc
IsaacAvila <izak.avila@gmail.com>
2017-02-19 01:39:19 -0600

    Exercise sql2, I added table employee_hobby, a catalogue between this table and the table employee and their respective inserts

--- employee_vauxoo.sql
+++ employee_vauxoo.sql
@@ -16,10 +16,22 @@ CREATE TABLE employee (
     id          SERIAL,
     first_name  VARCHAR(30) NOT NULL,
     last_name   VARCHAR(30) NOT NULL,
-    deapartment INT         REFERENCES employee_department(id) NOT NULL
+    department  INT         REFERENCES employee_department(id) NOT NULL
 );
 
+/* sql2 */
 CREATE TABLE employee_hobby (
+    PRIMARY KEY (id),
+    id          SERIAL,
+    name        VARCHAR(50) NOT NULL,
+    description TEXT        NOT NULL
+);
+
+/* Catalogue employee employee_hobby */
+CREATE TABLE employee_hobby_employee (
+	PRIMARY KEY (id_employee, id_hobby),
+    id_employee INT REFERENCES employee (id)       NOT NULL,
+    id_hobby    INT REFERENCES employee_hobby (id) NOT NULL
 );
 
 /* Inserts employee_department */
@@ -37,4 +49,21 @@ VALUES ('Juan', 'Camacho', 1),
        ('Mike',	'Wazowzki', 3),
        ('Noam', 'Chomsky', 5),
        ('Bugs', 'Bunny', 2);
+
+/* Inserts employee_hobby */
+INSERT INTO employee_hobby (name, description)
+VALUES ('Video Games', 'Spend time playing video games and become expert of each one of them'),
+       ('Sports', 'Spend time seeing or doing some kind of sport'),
+       ('Cook', 'You try to cook something delicious for everyone or just for yourself');
+
+/* Inserts employee_hobby_employee */
+INSERT INTO employee_hobby_employee (id_employee, id_hobby)
+VALUES (1, 2), -- Juan Camacho
+       (1, 3), 
+       (2, 1), -- Mike Wazowzki
+       (2, 3),
+       (3, 2), -- Noam Chomsky
+       (3, 3),
+       (4, 1), -- Bugs Bunny
+       (4, 2);
 
