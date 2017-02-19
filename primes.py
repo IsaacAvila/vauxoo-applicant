2185b47bf5909726aaab845f2dc2ad45b939e5c6
IsaacAvila <izak.avila@gmail.com>
2017-02-19 02:40:10 -0600

    Method for checking if a number is prime

--- primes.py
+++ primes.py
@@ -1,16 +1,27 @@
 """
-Your module documentation here
+I didn't use any module
 """
 
 
 class PrimeClass(object):
     """
-    Your class documentation here
+    Return a boolean with True for prime
+    and False for not prime
     """
 
     def is_prime(self, num_int):
         """
-        Your method documentation here
+        Uses a cycle that starts in 2
+        and does it until the middle of
+        the number, if it is divided with
+        some number returns false
         """
-        # your primes code here
-        return "not implement yet"  # Remove this dummy line
+        cont = 2
+        if num_int == 1:
+            return False
+        else:
+            while cont <= num_int / 2:
+                if num_int % cont == 0:
+                    return False
+                cont += 1
+            return True
